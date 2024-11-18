5a01a1fbcd61696045e5f675f773cdb2b4600794
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-11-18 13:00:42 +0100

    Add project files.

---
+++ drApi/Controllers/musicRecordsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace drApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class musicRecordsController : ControllerBase
+    {
+        private readonly musicRecordRepo _musicRecordRepo;
+        public musicRecordsController()
+        {
+            _musicRecordRepo = new musicRecordRepo();
+        }
+        // GET: api/<musicRecordsController>
+        [HttpGet]
+        public ActionResult<IEnumerable<musicRecord>> GetAll()
+        {
+            return _musicRecordRepo.GetAll().ToList();
+        }
+
+        // GET api/<musicRecordsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<musicRecordsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<musicRecordsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<musicRecordsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ drApi/musicRecordRepo.cs
@@ -0,0 +1,23 @@
+﻿namespace drApi
+{
+    public class musicRecordRepo
+    {
+        private int _nextId;
+        private readonly List<musicRecord> _musicRecords;
+        public musicRecordRepo()
+        {
+            _musicRecords = new List<musicRecord>()
+            {
+                new musicRecord { Id = 1, Title = "The Dark Side of the Moon", Artist = "Pink Floyd", Duration = 43, PublicationYear = 1973 },
+                new musicRecord { Id = 2, Title = "The Wall", Artist = "Pink Floyd", Duration = 81, PublicationYear = 1979 },
+                new musicRecord { Id = 3, Title = "Wish You Were Here", Artist = "Pink Floyd", Duration = 44, PublicationYear = 1975 }
+            };
+        }
+        public IEnumerable<musicRecord> GetAll()
+        {
+            IEnumerable<musicRecord> getAllList = new List<musicRecord>(_musicRecords);
+
+            return getAllList;
+        }
+    }
+}
