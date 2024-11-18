4065bbad57b5172c2bc701f55c3e78361011445b
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-11-18 13:11:02 +0100

    heihej

--- drApi/Program.cs
+++ drApi/Program.cs
@@ -1,20 +1,28 @@
+using drApi;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                              });
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+builder.Services.AddSingleton<musicRecordRepo>();
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseAuthorization();
 
